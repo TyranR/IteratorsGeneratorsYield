9998b242d0609f2554b5071950fb35526df0465f
me@tyran.ru <Defender2018for>
2019-11-06 01:07:10 +0300

    v.2

--- Iterator.py
+++ Iterator.py
@@ -1,22 +1,53 @@
-import json
+# Написать класс итератора, который по каждой стране из файла countries.json ищет страницу из википедии.
+# Записывает в файл пару: страна – ссылка. Ссылку формировать самостоятельно.
 
-class MyIter():
-    def __init__(self, start, end):
-        self.start, self.end = start - 1, end
+import json
+from pprint import pprint
+
+
+class MyIter:
+    def __init__(self, end, countries):
+        self.url = 'https://en.wikipedia.org/wiki/'
+        self.name = countries[0]
+        self.start = 0
+        self.end = end - 1
+        self.all = countries
 
     def __iter__(self):
         return self
 
     def __next__(self):
+        self.name = self.all[self.start]
+        prepare = self.name.split()
+        final = '_'.join(prepare)
+        self.url = 'https://en.wikipedia.org/wiki/' + final
         self.start += 1
         if self.start == self.end:
             raise StopIteration
-        return self.start
+        return self.name, self.url
+
+
+def countries_collect(data):
+    """
+    Собираем страны из JSON
+    """
+    all_countries = []
+    for each in data:
+        all_countries.append(each['name']['common'])
+    return all_countries
 
 
 def main():
-    pass
+    with open('countries.json', encoding='utf-8') as file:
+        json_data = json.load(file)
+    countries = countries_collect(json_data)
+    calc_countries = len(countries)
+    with open('result.txt', encoding='utf-8', mode='w') as file:
+        file.write("Let's do it!\n\n")
+    for item in MyIter(calc_countries, countries):
+        with open('result.txt', encoding='utf-8', mode='a') as file:
+            file.write(f'{item[0]} - {item[1]}\n')
+        # print(item)
 
 
-url = 'https://ru.wikipedia.org/wiki/'
 main()
