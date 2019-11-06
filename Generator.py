eb5b5de0fbfa013a2ce3600a7564ee13fdde875e
me@tyran.ru <Defender2018for>
2019-11-07 01:20:14 +0300

    v.3

--- Generator.py
+++ Generator.py
@@ -1,4 +1,9 @@
+# Написать генератор, который принимает путь к файлу.
+# При каждой итерации возвращает md5 хеш каждой строки файла.
+
 import hashlib
+from pprint import pprint
+
 
 def md5_func(mystring):
     """
@@ -6,3 +11,32 @@ def md5_func(mystring):
     """
     print
     return hashlib.md5(mystring.encode('utf-8')).hexdigest()
+
+
+def string_input():
+    """
+    Получаем файл или путь к файлу
+    """
+    # user_path = input("Введите путь к файлу: ")
+    user_path = 'result.txt'
+    return user_path
+
+
+def generator(file):
+    """
+    Итерируемся по каждой строке
+    """
+    for row in file:
+        yield row
+
+
+def main():
+    user_path = string_input()
+    my_file = open(user_path, mode='r', encoding='utf-8')
+    my_generator = generator(my_file)
+    for line in my_generator:
+        print(f"{line} = md5 {md5_func(line)}")
+    my_file.close()
+
+
+main()
